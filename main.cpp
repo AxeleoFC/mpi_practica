65868bf0f0556a14596052916981d1a12344e4f0
Axel Flores <axeleo2000@outlook.com>
2024-01-30 03:30:07 +0000

    frec colectiva con varios

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include <vector>
 #include <cstdlib>
 #include <mpi.h>
+#include <cmath>
 
-std::vector<int> frecuenciaCalcular(const std::vector<int>& valores, const std::vector<int>& numeros, int block_size) {
+#define CANT 17
+
+std::vector<int> frecuenciaCalcular(std::vector<int> valores, std::vector<int> numeros, int block_size, int resi) {
     std::vector<int> frecuencia_p(block_size, 0);
     for (int i = 0; i < block_size; ++i) {
-        for (int j = 0; j < numeros.size(); ++j) {
+        for (int j = 0; j < numeros.size()-resi; ++j) {
             if (valores[i] == numeros[j]) {
                 frecuencia_p[i] += 1;
             }
         }
     }
+    std::printf("\n");
     return frecuencia_p;
 }
 
@@ -24,65 +28,56 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int tamanioLista = 19;
-    std::vector<int> numeros(tamanioLista);
-    int block_size = (tamanioLista) / (nprocs);
+    int rows_per_rank;
+    int rows_alloc = CANT;
+    int padding = 0;
 
-    std::vector<int> valor(tamanioLista);
+    if (CANT % nprocs != 0) {
+        rows_alloc = std::ceil((double) CANT / nprocs) * nprocs;
+        padding = rows_alloc - CANT;
+    }
+    rows_per_rank = rows_alloc / nprocs;
+
+
+    std::vector<int> numeros(rows_alloc);
+
+    std::vector<int> valor(rows_alloc);
+    std::vector<int> frecuencia(rows_alloc);
 
     if (rank == 0) {
         std::printf("Rank_%d =", rank);
-        for (int i = 0; i < tamanioLista; ++i) {
-            int numero = rand() % 19;
-            std::printf("[%d],", numero);
+        for (int i = 0; i <= CANT; ++i) {
+            int numero = rand() % CANT;
             numeros[i] = numero;
             valor[i] = i;
         }
+        numeros[0]=16;
+        for (int i = 0; i <= CANT; ++i) {
+            std::printf("[%d],", numeros[i]);
+        }
         std::printf("\n");
-        for (int i = 1; i < nprocs; ++i) {
-            int inicio = i * block_size;
-            if (i == nprocs - 1) {
-                int residuo = tamanioLista % nprocs;
-                block_size = block_size + residuo;
-            }
-            MPI_Send(numeros.data(), tamanioLista, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&valor[inicio], block_size, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
+        MPI_Bcast(numeros.data(), rows_alloc, MPI_INT, 0, MPI_COMM_WORLD);
+    }
 
-        std::vector<std::vector<int>> frec_parciales(nprocs);
+    MPI_Bcast(numeros.data(), rows_alloc, MPI_INT, 0, MPI_COMM_WORLD);
+    std::vector<int> valor_p(rows_per_rank);
+    MPI_Scatter(valor.data(), rows_per_rank, MPI_INT, valor_p.data(), rows_per_rank, MPI_INT, 0, MPI_COMM_WORLD);
 
-        frec_parciales[0] = frecuenciaCalcular(valor, numeros, (tamanioLista) / (nprocs));
+    std::vector<int> frecu_p(rows_per_rank);
 
-        block_size = (tamanioLista) / (nprocs);
-        for (int i = 1; i < nprocs; ++i) {
-            if (i == nprocs - 1) {
-                int residuo = tamanioLista % nprocs;
-                block_size = block_size + residuo;
-            }
-            std::vector<int> temp_frec(block_size);
-            MPI_Recv(temp_frec.data(), block_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            frec_parciales[i] = temp_frec;
-        }
+    if(rank==nprocs-1){
+        frecu_p= frecuenciaCalcular(valor_p, numeros, rows_per_rank-padding, padding);
+    }else{
+        frecu_p= frecuenciaCalcular(valor_p, numeros, rows_per_rank, padding);
+    }
 
-        int indice=0;
-        for(std::vector<int> frec_par : frec_parciales){
-            for (int i = 0; i < frec_par.size(); ++i) {
-                if(frec_par[i]!=0){
-                    std::printf("Valor=%d Frecuencia=%d\n", valor[indice], frec_par[i]);
-                }
-                indice+=1;
+    MPI_Gather(frecu_p.data(), rows_per_rank, MPI_INT, frecuencia.data(), rows_per_rank, MPI_INT, 0, MPI_COMM_WORLD);
+    if(rank==0){
+        for(int i=0;i<frecuencia.size()-padding;++i){
+            if(frecuencia[i]!=0){
+                std::printf("Valor: %d frecuencia: %d\n",valor[i],frecuencia[i]);
             }
         }
-
-    } else {
-        if (rank == nprocs - 1) {
-            int residuo = tamanioLista % nprocs;
-            block_size = block_size + residuo;
-        }
-        MPI_Recv(numeros.data(), tamanioLista, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(valor.data(), block_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        std::vector<int> frecuencia_parcial = frecuenciaCalcular(valor, numeros, block_size);
-        MPI_Send(frecuencia_parcial.data(), block_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
